33f8873ee9a597fb3f273a7499658c990fcca5ae
Kashish Dange <kashishdange@gmail.com>
2024-12-31 00:49:48 +0530

    keywords added

--- sentiment_analy.py
+++ sentiment_analy.py
@@ -1,6 +1,6 @@
 def analyze_sentiment(text):
-    positive_keywords = ['great', 'fantastic', 'love', 'excellent', 'satisfied','good','great']
-    negative_keywords = ['terrible', 'bad', 'broke', 'not worth', 'hate','bad']
+    positive_keywords = ['great', 'fantastic', 'love', 'excellent', 'satisfied','good','great', 'nice','kind']
+    negative_keywords = ['terrible', 'bad', 'broke', 'not worth', 'hate','bad','not good']
 
     text_lower = text.lower()
 
